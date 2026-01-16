5b86e48ce9dd2cd4006b65042da56cb2469b80e8
Ubuntu <azureuser@Jenkins-Server-Terraform.0py2yuit1buu5npbmjoxndr22d.mx.internal.cloudapp.net>
2026-01-16 08:27:32 +0000

    Added Terraform code and Jenkinsfile updated

--- main.tf
+++ main.tf
@@ -1,4 +1,41 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "jenkins-terraform-rg"
+  name     = "jenkins-tf-rg"
   location = "centralindia"
 }
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "jenkins-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  lifecycle {
+    ignore_changes = [
+      tags,
+      size
+    ]
+  }
+}
+
